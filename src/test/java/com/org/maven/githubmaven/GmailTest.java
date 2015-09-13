264abd791e0448fad593bfc8d65fead5d228a6a6
bnanda <telljeet@gmail.com>
2015-09-13 07:48:20 +0100

    adding my maven project

---
+++ src/test/java/com/org/maven/githubmaven/GmailTest.java
@@ -0,0 +1,29 @@
+package com.org.maven.githubmaven;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+public class GmailTest {
+WebDriver driver;
+@Before
+public void setup()
+{
+driver = new FirefoxDriver();
+driver.get("http://www.gmail.com");
+}
+
+@Test
+public void Test()
+{
+driver.findElement(By.id("Email")).sendKeys("Test@test.com");
+}
+
+@After
+public void TearDown()
+{
+driver.quit();
+}
+}
